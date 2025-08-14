24e249de4085c8e4696824459fdcc77927543b80
Verthie <vertisixs@gmail.com>
2025-08-14 20:36:32 +0200

    chore: added initial controllers

---
+++ Controllers/TreeReportsController.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrzewaAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TreeReportsController : ControllerBase
+{
+
+}
